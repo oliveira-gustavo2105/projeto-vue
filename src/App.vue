780ba6f6af90c13859d7392b74b8ec0bab4900fe
gustavo <gugatenere@gmail.com>
2021-03-22 11:51:15 -0300

    primeira impoetacao

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+
+       <h2 id="cliente-title">  Clientes </h2>
+    <h3> INFORMAÇÕES SOBRE OS CLIENTES </h3>
+
+   
+         
+         
+
+    <br><br>
+      <Cliente/>
+      <br><br>
+      <Cliente/>
+      <br><br>
+      <Cliente/>
+      <br><br>
+      
+      
+      
+    
+  </div>
+</template>
+
+<script>
+
+
+import Cliente from './components/Cliente';
+
+export default {
+  name: 'App',
+    data(){
+      return{
+
+      
+
+          nome:"",
+          profissao:"",
+          endereco:"",
+          email:"",
+          indicacao:""
+          
+
+      }
+
+      
+    },
+        
+      components: {
+
+      Cliente
+
+    }
+
+
+    }
+
+    
+
+    
+
+
+    
+
+  
+  
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Cliente.vue
@@ -0,0 +1,95 @@
+<template>
+    
+    
+    <div :class="{'cliente': plus, 'cliente-plus': !plus }">
+    
+    
+
+        
+        <input type="text" v-model="nome" placeholder="Nome">
+         <input type="text" v-model="profissao" placeholder="Profissão">
+         <input type="text" v-model="endereco" placeholder="Local de Trabalho">
+         <input type="email" v-model="email" placeholder="E-mail">
+         <input type="text" v-model="indicacao" placeholder="Indicação">
+
+         <button @click="trocaPlus">Plus</button>
+         
+         
+        
+        <h2>Nome: {{nome}} </h2>
+     
+     <hr>
+        <h2>Profissão: {{profissao}}</h2>
+        
+        <hr>
+           
+        <h2>Empresa: {{endereco}}</h2>
+        
+        <hr>
+                       
+        <h2>E-mail: {{email}}</h2>
+        <hr>
+                    
+        <h2>Indicação: {{indicacao}}</h2>
+        <hr>
+        <br> <br>
+
+        
+
+    </div>
+    
+    
+        
+
+
+   
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+            nome: "",
+            profissao: "",
+            endereco: "",
+            email: "",
+            indicacao: "",
+            plus:Boolean,
+            
+        }
+    },
+    methods: {
+        trocaPlus: function(){
+
+            this.plus = !this.plus;
+        }
+
+
+    }
+  
+  
+  }
+</script>
+
+<style scoped>
+
+    .cliente{
+        color: purple;
+        background-color: #f0fff0;
+        
+
+    }
+
+   
+
+    .cliente-plus{
+
+        color: blueviolet;
+        background-color: navajowhite;
+
+    }
+
+
+</style>>
+
